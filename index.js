f5c1f9a5b8fac6467fb1aaa68fef7c3ea47c8499
Hanna Lee <mani21na@gmail.com>
2020-02-28 18:14:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
-  let left = parseInt(rightNumbers, 10);
+  let right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
     dodger.style.right = `${right - 1}px`;
