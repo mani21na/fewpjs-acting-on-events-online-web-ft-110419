df12baaab825523a2f97cbb9675ddc1e40d541b6
Hanna Lee <mani21na@gmail.com>
2020-02-28 18:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ dodger.style.bottom = "100px";
 
 dodger.style.bottom = "0px";
 
-document.addEventListener("keydown", function(e)){
-  console.log(e.key);
-}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
+});
