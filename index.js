de99e826f942b2c85324a88e5f9c61e2007ac689
Hanna Lee <mani21na@gmail.com>
2020-02-28 18:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,3 +43,9 @@ function moveDodgerRight() {
     dodger.style.right = `${right - 1}px`;
   }
 }
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerLeft();
+  }
+});
