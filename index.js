1177fd4acc89f0f0133c1317d5364800cbee0442
Hanna Lee <mani21na@gmail.com>
2020-02-28 18:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
 // Your code here
 let dodger = document.getElementById("dodger");
-
-dodger.style.backgroundColor = "#000000";
-
-dodger.style.backgroundColor = "#FF69B4";
-
-dodger.style.left;
-dodger.style.bottom;
-dodger.style.bottom = "100px";
-
+dodger.style.backgroundColor = "pink";
 dodger.style.bottom = "0px";
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
+function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
 
-    dodger.style.left = `${left - 1}px`;
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
   }
-});
 
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
   }
-}
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
-
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
-  }
-}
-
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
-});
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
