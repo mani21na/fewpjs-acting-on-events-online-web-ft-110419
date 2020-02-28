b51885e8d7ff4c969e097d2dc92bce9501a133f8
Hanna Lee <mani21na@gmail.com>
2020-02-28 18:11:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ document.addEventListener("keydown", function(e) {
     dodger.style.left = `${left - 1}px`;
   }
 });
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
